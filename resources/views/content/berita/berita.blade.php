d7b35ba4ca1e1f9aad7a843f20388dbce1cf6b52
angella6969 <irvan_arifafandi@yahoo.com>
2023-11-01 16:35:57 +0700

    a

--- resources/views/content/berita/berita.blade.php
+++ resources/views/content/berita/berita.blade.php
@@ -10,7 +10,12 @@
             <div class="row">
                 <div class="col-8 mt-3" data-aos="zoom-out">
                     <div class="d-flex justify-content-center" data-aos="zoom-out">
-                        <img style="height: 40vh" width="100%" src="{{ asset('images\image\dance_of_lights_by_ellysiumn_ddanutv-350t.jpg') }}" alt="">
+                        <img style="height: 40vh" width="100%"
+                            src="{{ asset('images\image\dance_of_lights_by_ellysiumn_ddanutv-350t.jpg') }}" alt="">
+                    </div>
+                    <div>
+                        <label for="">admin</label>
+                        <label for="">awdawdad</label>
                     </div>
                     <div class="col-lg-9 text-center text-lg-start mt-3 mb-3" style="margin-left: 30px">
                         <h3>Call To Action</h3>
@@ -18,25 +23,32 @@
                             cillum dolore eu fugiat nulla
                             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
                             mollit anim id est laborum.</p>
-                        <p style="text-align: justify"> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                        <p style="text-align: justify"> Duis aute irure dolor in reprehenderit in voluptate velit esse
+                            cillum dolore eu fugiat nulla
                             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
                             mollit anim id est laborum.</p>
-                        <p style="text-align: justify"> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                        <p style="text-align: justify"> Duis aute irure dolor in reprehenderit in voluptate velit esse
+                            cillum dolore eu fugiat nulla
                             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
                             mollit anim id est laborum.</p>
-                        <p style="text-align: justify"> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                        <p style="text-align: justify"> Duis aute irure dolor in reprehenderit in voluptate velit esse
+                            cillum dolore eu fugiat nulla
                             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
                             mollit anim id est laborum.</p>
-                        <p style="text-align: justify"> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                        <p style="text-align: justify"> Duis aute irure dolor in reprehenderit in voluptate velit esse
+                            cillum dolore eu fugiat nulla
                             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
                             mollit anim id est laborum.</p>
-                        <p style="text-align: justify"> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                        <p style="text-align: justify"> Duis aute irure dolor in reprehenderit in voluptate velit esse
+                            cillum dolore eu fugiat nulla
                             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
                             mollit anim id est laborum.</p>
-                        <p style="text-align: justify"> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                        <p style="text-align: justify"> Duis aute irure dolor in reprehenderit in voluptate velit esse
+                            cillum dolore eu fugiat nulla
                             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
                             mollit anim id est laborum.</p>
-                        <p style="text-align: justify"> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                        <p style="text-align: justify"> Duis aute irure dolor in reprehenderit in voluptate velit esse
+                            cillum dolore eu fugiat nulla
                             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
                             mollit anim id est laborum.</p>
                     </div>
--- resources/views/layout/content/footer.blade.php
+++ resources/views/layout/content/footer.blade.php
@@ -1,4 +1,5 @@
  <!-- ======= Footer ======= -->
+
  <footer id="footer">
      <div class="container">
          <div class="row">
@@ -33,83 +34,73 @@
 
          </div>
 
-         {{-- <div class="mobil position-absolute  translate-middle"
-         style=" 
-            width: 100px;
-            height: 100px;
-            background-image: url('../images/truck-3.png');
-            background-size: contain;
-            background-repeat: no-repeat;
-            position: absolute;
-            margin-bottom: 60px;
-            z-index:999;
-            left: 0;">
-     </div>
-     <div class="mobil position-absolute  translate-middle"
-         style=" 
-            width: 100px;
-            height: 100px;
-            background-image: url('../images/truck-2.png');
-            background-size: contain;
-            background-repeat: no-repeat;
-            position: absolute;
-            margin-bottom: 60px;
-            z-index:999;
-            left: 0;">
-     </div>
-     <div class="mobil position-absolute"
-         style=" 
-            width: 100px;
-            height: 100px;
-            background-image: url('../images/truck-1.png');
-            background-size: contain;
-            background-repeat: no-repeat;
-            position: absolute;
-            margin-bottom: 15px;
-            z-index:999;
-            left: 0;">
-     </div> --}}
-
-
      </div>
 
-     
-     <div class="copyright mt-3">
-         Hak Cipta &copy;2023 <strong><span>Sisda Balai Besar Wilayah Sungai Serayu Opak</span></strong> | Ditjen
-         Sumber
-         Daya Air | Kementerian Pekerjaan Umum dan Perumahan Rakyat Republik Indonesia, All Rights Reserved
+
+
      </div>
  </footer><!-- End Footer -->
+ <div class="footer2">
+     <div class="row">
+         <div class="mobil"
+             style=" 
+       width: 100px;
+       height: 100px;
+       background-image: url('../images/truck-1.png');
+       background-size: contain;
+       background-repeat: no-repeat;
+       /* position: absolute; */
+       background-position:bottom;
+       z-index:999;">
+         </div>
+         <div class="mobil"
+             style=" 
+       width: 100px;
+       height: 100px;
+       background-image: url('../images/truck-2.png');
+       background-size: contain;
+       background-repeat: no-repeat;
+       position: absolute;
+       background-position:bottom;
+       z-index:999;">
+         </div>
+     </div>
+ </div>
+ <div class="copyright d-flex justify-content-center footer3">
+     Hak Cipta &copy;2023 <strong><span>Sisda Balai Besar Wilayah Sungai Serayu Opak</span></strong> | Ditjen
+     Sumber
+     Daya Air | Kementerian PUPR. All Rights Reserved
+ </div>
 
 
 
- {{-- <script>
-     var mobil1 = document.querySelectorAll('.mobil')[0];
+ <script>
+     //  var mobil1 = document.querySelectorAll('.mobil')[0];
      var mobil2 = document.querySelectorAll('.mobil')[1];
-     var mobil3 = document.querySelectorAll('.mobil')[2];
-     var posisi1 = 0;
+     var mobil3 = document.querySelectorAll('.mobil')[0];
+     //  var posisi1 = 0;
      var posisi2 = window.innerWidth - 100; // Mengatur posisi awal elemen kedua
      var posisi3 = window.innerWidth - 100; // Mengatur posisi awal elemen kedua
-     var kecepatan1 = 2;
-     var kecepatan2 = 3;
-     var kecepatan3 = 1;
-     var arah1 = 1;
+     //  var kecepatan1 = 6;
+     var kecepatan2 = 13;
+     var kecepatan3 = 10;
+     //  var arah1 = 1;
      var arah2 = -1; // Arah elemen kedua berlawanan dengan elemen pertama
      var arah3 = -1; // Arah elemen kedua berlawanan dengan elemen pertama
 
      function animasi() {
-         posisi1 += kecepatan1 * arah1;
+         //  posisi1 += kecepatan1 * arah1;
          posisi2 += kecepatan2 * arah2;
          posisi3 += kecepatan3 * arah3;
-         mobil1.style.left = posisi1 + "px";
+         //  mobil1.style.left = posisi1 + "px";
          mobil2.style.left = posisi2 + "px";
          mobil3.style.left = posisi3 + "px";
 
          // Memantulkan elemen pertama saat mencapai batas tertentu
-         if (posisi1 >= window.innerWidth - mobil1.offsetWidth || posisi1 <= 0) {
-             arah1 = -arah1;
-             mobil1.style.transform = 'scaleX(' + -arah1 + ')';
-         }
+         //  if (posisi1 >= window.innerWidth - mobil1.offsetWidth || posisi1 <= 0) {
+         //      arah1 = -arah1;
+         //      mobil1.style.transform = 'scaleX(' + -arah1 + ')';
+         //  }
 
          // Memantulkan elemen kedua saat mencapai batas tertentu
          if (posisi2 >= window.innerWidth - mobil2.offsetWidth || posisi2 <= 0) {
@@ -126,4 +117,4 @@
      }
 
      animasi();
- </script> --}}
+ </script>
