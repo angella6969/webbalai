44b238a208fc1e1ebb67f349b8a36ae3fe1961d0
angella6969 <irvan_arifafandi@yahoo.com>
2023-11-29 16:32:35 +0700

    awd 2awd1awd

--- resources/views/components/h_content.blade.php
+++ resources/views/components/h_content.blade.php
@@ -1,10 +1,10 @@
-<section id="header-1" class="breadcrumbs">
+<section class="breadcrumbs">
     <div class="container">
         <div class="d-flex justify-content-between align-items-center">
-            <h2 style="color:var(--bs-bg-text);">{{ $judul }}</h2>
+            <h2>{{ $judul }}</h2>
             <ol>
-                <li><a style="color:var(--bs-bg-text);" href="/">{{ $posisi1 }}</a></li>
-                <li style="color:var(--bs-bg-text);">{{ $posisi2 }}</li>
+                <li><a href="/">{{ $posisi1 }}</a></li>
+                <li>{{ $posisi2 }}</li>
             </ol>
         </div>
 
