55f4c0bc4abc843302c48a94080618e6b950a02b
angella6969 <irvan_arifafandi@yahoo.com>
2024-02-13 23:32:40 +0700

    fix 2

--- resources/views/layout/content/link.blade.php
+++ resources/views/layout/content/link.blade.php
@@ -1,13 +1,13 @@
 <!-- Vendor CSS Files -->
-<link href="{{assets/vendor/animate.css/animate.min.css}}" rel="stylesheet">
-<link href="{{assets/vendor/aos/aos.css}}" rel="stylesheet">
-<link href="{{assets/vendor/bootstrap/css/bootstrap.min.css}}" rel="stylesheet">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css}}">
-<link href="{{assets/vendor/boxicons/css/boxicons.min.css}}" rel="stylesheet">
-<link href="{{assets/vendor/glightbox/css/glightbox.min.css}}" rel="stylesheet">
-<link href="{{assets/vendor/remixicon/remixicon.css}}" rel="stylesheet">
-<link href="{{assets/vendor/swiper/swiper-bundle.min.css}}" rel="stylesheet">
-<link href="{{assets/css/style.css}}" rel="stylesheet">
+<link href="{{'assets/vendor/animate.css/animate.min.css'}}" rel="stylesheet">
+<link href="{{'assets/vendor/aos/aos.css'}}" rel="stylesheet">
+<link href="{{'assets/vendor/bootstrap/css/bootstrap.min.css'}}" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+<link href="{{'assets/vendor/boxicons/css/boxicons.min.css'}}" rel="stylesheet">
+<link href="{{'assets/vendor/glightbox/css/glightbox.min.css'}}" rel="stylesheet">
+<link href="{{'assets/vendor/remixicon/remixicon.css'}}" rel="stylesheet">
+<link href="{{'assets/vendor/swiper/swiper-bundle.min.css'}}" rel="stylesheet">
+<link href="{{'assets/css/style.css'}}" rel="stylesheet">
 
 <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
 <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css" />
--- resources/views/layout/content/script.blade.php
+++ resources/views/layout/content/script.blade.php
@@ -1,14 +1,14 @@
 <!-- Vendor JS Files -->
-<script src="{{ assets/vendor/aos/aos.js }}"></script>
+<script src="{{ 'assets/vendor/aos/aos.js '}}"></script>
 
-<script src="{{ assets/vendor/bootstrap/js/bootstrap.bundle.min.js }}"></script>
-<script src="{{ assets/vendor/glightbox/js/glightbox.min.js }}"></script>
-<script src="{{ assets/vendor/isotope-layout/isotope.pkgd.min.js }}"></script>
-<script src="{{ assets/vendor/swiper/swiper-bundle.min.js }}"></script>
-<script src="{{ assets/vendor/php-email-form/validate.js }}"></script>
+<script src="{{ 'assets/vendor/bootstrap/js/bootstrap.bundle.min.js '}}"></script>
+<script src="{{ 'assets/vendor/glightbox/js/glightbox.min.js '}}"></script>
+<script src="{{ 'assets/vendor/isotope-layout/isotope.pkgd.min.js '}}"></script>
+<script src="{{ 'assets/vendor/swiper/swiper-bundle.min.js '}}"></script>
+<script src="{{ 'assets/vendor/php-email-form/validate.js '}}"></script>
 
 <!-- Template Main JS File -->
-<script src="{{ assets/js/main.js }}"></script>
+<script src="{{ 'assets/js/main.js '}}"></script>
 <script src="https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js"></script>
 <script src="https://unpkg.com/feather-icons"></script>
 {{-- <script src="{{ asset('assets/js/owl.carousel.min.js') }}"></script> --}}
